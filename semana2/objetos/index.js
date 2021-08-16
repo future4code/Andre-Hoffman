49c95575bea88b82c24e783167e47b7309057a07
André Leal Hoffman <andrehoffman@poli.ufrj.br>
2021-08-16 16:48:29 -0300

    apenas exerc. interpre. código

---
+++ semana2/objetos/index.js
@@ -0,0 +1,20 @@
+/*
+1)a)
+"Matheus Nachtergaele"
+"Virginia Cavendish"
+{canal: "Globo", horario: "14h"}
+2)a)
+{nome:"Juca",idade: 3,raca:"SRD"}
+{nome:"Juba",idade: 3,raca:"SRD"}
+{nome:"Juco",idade: 3,raca:"SRD"}
+
+b)Essa sintaxe se chama espalhamento e realiza
+uma cópia do objetivo que se quer para outro,
+possibilitando reajustar posteriormente as informações.
+
+3)a)
+false
+b)
+o return pega o objeto, seguido de valor da chave backender, indicando falso
+
+*/

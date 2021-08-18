3949779172825011321572ccecee2b44c33766c2
André Leal Hoffman <andrehoffman@poli.ufrj.br>
2021-08-18 16:38:38 -0300

    quarta, primeira parte

---
+++ semana2/loops/index.js
@@ -0,0 +1,20 @@
+/*
+1)Iterando de i=0 até i=4, fazendo a soma 0+1+2+3+5, imprimindo 11 ao final
+
+2)a)
+Serão impressos os números 19,21,23,25,27,30
+
+b)Poderíamos fazer:
+let index
+for(let numero of lista){
+    index = lista.indexOf(numero)
+}
+
+3)
+
+"*"
+"**"
+"***"
+"****"
+
+*/

262812af5f23d88d826478b2e4ced533f93d8497
André Leal Hoffman <andrehoffman@poli.ufrj.br>
2021-09-13 23:47:40 -0300

    entrega incompleta

---
+++ semana6/LabenuForms/labenuforms/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Etapa1 from "./components/Etapa1.js"
+
+export default class App extends React.Component {
+  render() {
+      return (
+          <div>Olá, você está App.js</div>
+      )
+  }
+}
---
+++ semana6/LabenuForms/labenuforms/src/components/Etapa1.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Etapa1 extends React.Component {
+    render() {
+        return (
+            <div>Olá, você está na Etapa1</div>
+        )
+    }
+}

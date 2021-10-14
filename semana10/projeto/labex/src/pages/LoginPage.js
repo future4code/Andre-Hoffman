4a25c324ca97ce97476d4e2c5a5eec079bb20e9f
André Leal Hoffman <andrehoffman@poli.ufrj.br>
2021-10-14 18:05:01 -0300

    14.10

--- semana10/projeto/labex/src/pages/LoginPage.js
+++ semana10/projeto/labex/src/pages/LoginPage.js
@@ -1,7 +1,13 @@
 import React from "react";
-import { useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom";
+import {useState} from "react"
+import axios from "axios";
+import { getDefaultNormalizer } from "@testing-library/react";
 
 export default function LoginPage() {
+
+    const [email,setEmail] = useState("");
+    const [password, setPassword] = useState("")
 
     const history = useHistory();
 
@@ -17,9 +23,50 @@ export default function LoginPage() {
         history.push("/admin/trips/:id");
       };
 
+      const onChangeEmail = (event) =>{
+        setEmail(event.target.value);
+      }
+
+      const onChangePassword = (event) => {
+        setPassword(event.target.value);
+      }
+
+      const onSubmitLogin = () => {
+        console.log(email, password);
+        const body = {
+          email: email,
+          password: password
+
+        }
+        axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labeX/andre-leal-maryam/login", body)
+        .then((response)=>{
+          console.log(response.data.token)
+          localStorage.setItem('token', response.data.token)
+        }).catch((error)=>{
+          console.log(error.response)
+        })
+        
+      }
+      // https://us-central1-labenu-apis.cloudfunctions.net/labeX/:aluno/trips
+      // https://us-central1-labenu-apis.cloudfunctions.net/labeX/:aluno/trip/:id
+
+
     return (
         <div>
             <h1>Admin Area</h1>
+            <input
+              placeholder = "email"
+              type="email"
+              value={email}
+              onChange={onChangeEmail}
+            />
+            <input
+              placeholder = "password"
+              type="password"
+              value={password}
+              onChange={onChangePassword}
+            />
+            <button onClick={onSubmitLogin}>Enviar</button>
            <button onClick={goToAdminHomePage}>Go to Admin Home Page</button>
            <button onClick={goToCreateTrip}>Create new Trips</button>
            <button onClick={goToTripDetails}>Go to trip details</button>
--- semana10/projeto/labex/src/pages/TripDetailsPage.js
+++ semana10/projeto/labex/src/pages/TripDetailsPage.js
@@ -1,9 +1,25 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 export default function TripDetailsPage() {
+
+    useEffect(()=>{
+        axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labeX/andre-leal-maryam/trip/6AF296jholXttVSwuHNB",{ 
+        headers: {
+            auth: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkRCQjVic0MyczFacjM4UEVUZk5DIiwiZW1haWwiOiJhbmRyZWxlYWxAZ21haWwuY29tLmJyIiwiaWF0IjoxNjM0MjM0MzQ3fQ.PI-aMIqceRFElFj32V3CKVK6u4ZbYtC7FoifIBwkMGQ'
+        }})
+         .then((response)=>{
+             console.log(response.data)
+         }).catch((error)=>{
+             console.log(error.response)
+         })
+    }, [])
     return (
         <div>
             <h1>TripDetailsPage</h1>
         </div>
     );
 }
+//https://us-central1-labenu-apis.cloudfunctions.net/labeX/:aluno/trip/:id
+//"6AF296jholXttVSwuHNB"
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkRCQ…DEzfQ.OUSqjEZ-8GP0QyXn1jQ7GMUnD45rbhpbBYw72z9j7xk

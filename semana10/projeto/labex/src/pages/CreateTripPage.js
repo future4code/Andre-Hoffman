97ae5311ad91bbdc5fb2459d862b7dc88ef7e2a9
André Leal Hoffman <andrehoffman@poli.ufrj.br>
2021-10-14 23:41:08 -0300

    14.10

--- semana10/projeto/labex/src/pages/CreateTripPage.js
+++ semana10/projeto/labex/src/pages/CreateTripPage.js
@@ -1,6 +1,38 @@
 import React from "react";
+import axios from "axios";
+import { useHistory } from "react-router";
+
+const useProtectedPage = () => {
+    const history = useHistory()
+
+    useEffect(()=>{
+        const token = localStorage.getItem("token");
+        if(token === null) {
+            console.log("Não está logado")
+            history.push('/login')
+        }
+    }, [])
+}
 
 export default function CreateTripPage() {
+    
+    useProtectedPage()
+
+    useEffect(()=>{
+        const token = localStorage.getItem(
+            'token'
+        )
+        axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labeX/andre-leal-maryam/trip/6AF296jholXttVSwuHNB",{ 
+        headers: {
+            auth: token
+        }})
+         .then((response)=>{
+             console.log(response.data)
+         }).catch((error)=>{
+             console.log(error.response)
+         })
+    }, [])
+
     return (
         <div>
            <h1> CreateTripPage</h1>
--- semana10/projeto/labex/src/pages/TripDetailsPage.js
+++ semana10/projeto/labex/src/pages/TripDetailsPage.js
@@ -1,12 +1,30 @@
-import axios from "axios";
 import React, { useEffect } from "react";
+import axios from "axios";
+import { useHistory } from "react-router";
+
+const useProtectedPage = () => {
+    const history = useHistory()
+
+    useEffect(()=>{
+        const token = localStorage.getItem("token");
+        if(token === null) {
+            console.log("Não está logado")
+            history.push('/login')
+        }
+    }, [])
+}
 
 export default function TripDetailsPage() {
 
+    useProtectedPage()
+
     useEffect(()=>{
+        const token = localStorage.getItem(
+            'token'
+        )
         axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labeX/andre-leal-maryam/trip/6AF296jholXttVSwuHNB",{ 
         headers: {
-            auth: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkRCQjVic0MyczFacjM4UEVUZk5DIiwiZW1haWwiOiJhbmRyZWxlYWxAZ21haWwuY29tLmJyIiwiaWF0IjoxNjM0MjM0MzQ3fQ.PI-aMIqceRFElFj32V3CKVK6u4ZbYtC7FoifIBwkMGQ'
+            auth: token
         }})
          .then((response)=>{
              console.log(response.data)
@@ -20,6 +38,3 @@ export default function TripDetailsPage() {
         </div>
     );
 }
-//https://us-central1-labenu-apis.cloudfunctions.net/labeX/:aluno/trip/:id
-//"6AF296jholXttVSwuHNB"
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkRCQ…DEzfQ.OUSqjEZ-8GP0QyXn1jQ7GMUnD45rbhpbBYw72z9j7xk
